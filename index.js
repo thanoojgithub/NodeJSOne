b8c679b48fa99bdb5dc9330f25a2832501924cea
thanooj kalathuru <javaeecub@gmail.com>
2022-06-30 11:40:05 +0530

    Changed JSON data and API code respectively

--- index.js
+++ index.js
@@ -12,59 +12,77 @@ let db = new Low(adapter)
 
 console.log(db);
 await db.read()
-db.data ||= { posts: [] , notes: []}
-let { posts } = db.data
-console.log(posts);
+db.data ||= { phoneList: [] , notes: []}
+let { phoneList } = db.data
+console.log("phoneList", phoneList);
 let { notes } = db.data
 console.log(notes);
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 4000
 
-app.get('/posts/', async (req, res) => {
+
+//GET all phones
+app.get('/phones/', async (req, res) => {
     console.log(req.baseUrl)
-    res.json(posts)
+    res.json(phoneList)
 })
 
-app.get('/posts/id/:id', async (req, res) => {
-    console.log("eq.params.id", req.params.id)
-    const post = posts.find((p) => p.id === req.params.id)
-    if (!post) return res.sendStatus(404)
-    console.log("--------------------------")
-    console.log("/posts/:id", post)
-    console.log("--------------------------")
-    res.json(post)
+// GET all phones [{id,name}]
+app.get('/phones/names', async (req, res) => {
+    console.log(req.baseUrl)
+    phoneList.forEach(function(elem) {
+        console.log(elem.id + " , "+ elem.name);
+    });
+    var pJSONs = function() { 
+        return phoneList.map(function(pJSON) {
+            return {id : pJSON.id, name : pJSON.name} 
+          })
+    };
+    console.log(pJSONs());
+    res.json(pJSONs())
 })
 
-
-app.get('/posts/msg/:msg', async (req, res) => {
-    console.log("eq.params.msg", req.params.msg)
-    console.log(posts)
-    /* const post = posts.find( p => p.msg === req.params.msg) 
-    const post = posts.find((p) => "/^.*"+req.params.msg+".*$/".test(p.msg))*/
-  
-    const post = posts.filter((p) => p.msg.includes(req.params.msg))
-    if (!post) return res.sendStatus(404) 
+// GET a phone by ID
+app.get('/phones/id/:id', async (req, res) => {
+    console.log("req.params.id", req.params.id)
+    const phone = phoneList.find((p) => p.id === req.params.id)
+    if (!phone) return res.sendStatus(404)
     console.log("--------------------------")
-    console.log("/posts/:msg", post)
+    console.log("/phones/:id", phone)
     console.log("--------------------------")
-    res.json(post)
+    res.json(phone)
 })
 
-app.post('/posts', async (req, res, next) => {
+// GET a phone by name (fuzzy search) 
+app.get('/phones/name/:name', async (req, res) => {
+    console.log("req.params.name", req.params.name)
+    console.log(phoneList)
+    /* const phones = phoneList.filter((p) => p.name.includes(req.params.name) || p.model.includes(req.params.name)) */
+    const phones = phoneList.filter((p) => p.name.toLowerCase().includes(req.params.name.toLowerCase()))
+    if (!phones) return res.sendStatus(404)
+    console.log("--------------------------")
+    console.log("/phones/:name", phones)
+    console.log("--------------------------")
+    res.json(phones)
+})
+
+// POST a new phone JSON data
+app.post('/phones', async (req, res) => {
     console.log("req.body", req.body)
-    console.log("db.data.posts", posts)
-    const post = posts.push(req.body)
+    console.log("db.data.phoneList", phoneList)
+    const post = phoneList.push(req.body)
     await db.write()
     res.sendStatus(200)
-  })
+})
   
 
+
+// Express App up & running
+const PORT = 4000
 app.listen(PORT, () => {
     console.log("Backend is running on http://localhost:"+PORT)
 })
