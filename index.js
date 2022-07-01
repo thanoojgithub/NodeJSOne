c4fb6612b55a6ab0fc3bfca20e2048b6439d6f66
thanooj kalathuru <javaeecub@gmail.com>
2022-07-01 11:59:28 +0530

    added morgan & brand related changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import express from 'express'
 import { Low, JSONFile } from 'lowdb'
 import cors from 'cors'
 import { fileURLToPath } from 'url'
-
+import morgan from 'morgan'
+import rfs from 'rotating-file-stream'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const dbFile = join(__dirname, 'db.json')
@@ -12,23 +13,33 @@ let db = new Low(adapter)
 
 console.log(db);
 await db.read()
-db.data ||= { phoneList: [] , notes: []}
+db.data ||= { phoneList: [] , brands: []}
 let { phoneList } = db.data
 console.log("phoneList", phoneList);
-let { notes } = db.data
-console.log(notes);
+let { brands } = db.data
+console.log(brands);
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+const logStream = rfs.createStream("access.log", {
+    size: "1M", // rotate every 10 MegaBytes written
+    interval: "1d", // rotate daily
+    compress: "gzip" // compress rotated files
+  });
+app.use(morgan(':date - :method :url - status :status, res-content-length :res[content-length] Bytes, process-time :total-time ms', { stream: logStream }))
 
 
 
 //GET all phones
 app.get('/phones/', async (req, res) => {
-    console.log(req.baseUrl)
     res.json(phoneList)
+})
+
+//GET all brands
+app.get('/brands/', async (req, res) => {
+    res.json(brands)
 })
 
 // GET all phones [{id,name}]
@@ -57,6 +68,18 @@ app.get('/phones/id/:id', async (req, res) => {
     res.json(phone)
 })
 
+// GET a phone by Brand-ID
+app.get('/phones/brand/:brand_id', async (req, res) => {
+    console.log("req.params.brand_id", req.params.brand_id)
+    const phone = phoneList.filter((p) => p.brand_id === req.params.brand_id)
+    if (!phone) return res.sendStatus(404)
+    console.log("--------------------------")
+    console.log("/phones/:id", phone)
+    console.log("--------------------------")
+    res.json(phone)
+})
+
+
 // GET a phone by name (fuzzy search) 
 app.get('/phones/name/:name', async (req, res) => {
     console.log("req.params.name", req.params.name)
@@ -81,8 +104,8 @@ app.post('/phones/phone', async (req, res) => {
   
 
 
-// Express App up & running
-const PORT = 4000
+// Node + Express Application is up and running
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log("Backend is running on http://localhost:"+PORT)
 })
